474811d75a41283ef1b48b028a79230048eb3b67
Akash <akashbhattacharyak1314@gmail.com>
2023-01-26 11:40:02 +0530

    adding location_activity

--- app/src/main/java/com/example/teamremoteopc/MainActivity.kt
+++ app/src/main/java/com/example/teamremoteopc/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.teamremoteopc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.teamremoteopc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    private fun navigateToLocation() {
+        val intent = Intent(this@MainActivity, LocationActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.location_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.location_activity -> navigateToLocation()
+        }
+
+        return true
+    }
+
 }
